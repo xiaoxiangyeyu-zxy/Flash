be14614d79d41262befd4757fdb5f9e623fe6425
xiaoxiangyeyu-zxy <74137364+xiaoxiangyeyu-zxy@users.noreply.github.com>
2022-12-17 23:32:14 +0800

    Add files via upload

---
+++ test_nolaser_two_slab_heat_constant/Simulation_initBlock.F90
@@ -0,0 +1,150 @@
+!!****if* source/Simulation/SimulationMain/LaserSlab/Simulation_initBlock
+!!
+!! NAME
+!!
+!!  Simulation_initBlock
+!!
+!!
+!! SYNOPSIS
+!!
+!!  call Simulation_initBlock(integer(IN) :: blockID) 
+!!                       
+!!
+!!
+!!
+!! DESCRIPTION
+!!
+!!  Initializes fluid data (density, pressure, velocity, etc.) for
+!!  a specified block.
+!! 
+!! ARGUMENTS
+!!
+!!  blockID -        the number of the block to initialize
+!!  
+!!
+!!
+!!***
+
+subroutine Simulation_initBlock(blockId)
+  use Simulation_data
+  use Grid_interface, ONLY : Grid_getBlkIndexLimits, &
+       Grid_getCellCoords, Grid_putPointData
+
+  use Driver_interface, ONLY: Driver_abortFlash
+  use RadTrans_interface, ONLY: RadTrans_mgdEFromT
+
+  implicit none
+
+#include "constants.h"
+#include "Flash.h"
+
+  ! compute the maximum length of a vector in each coordinate direction 
+  ! (including guardcells)
+
+  integer, intent(in) :: blockId
+  
+  integer :: i, j, k, n
+  integer :: blkLimits(2, MDIM)
+  integer :: blkLimitsGC(2, MDIM)
+  integer :: axis(MDIM)
+  real, allocatable :: xcent(:), ycent(:), zcent(:)
+  real :: tradActual
+  real :: rho, tele, trad, tion, zbar, abar, vx
+  integer :: species
+  real, pointer, dimension(:,:,:,:) :: facexData,faceyData
+#if NDIM > 0
+  real, pointer, dimension(:,:,:,:) :: facezData
+#endif
+
+#ifndef CHAM_SPEC
+  integer :: CHAM_SPEC = 1, TARG_SPEC = 2
+#endif
+
+
+  ! get the coordinate information for the current block from the database
+  call Grid_getBlkIndexLimits(blockId,blkLimits,blkLimitsGC)
+
+  ! get the coordinate information for the current block from the database
+  call Grid_getBlkIndexLimits(blockId,blkLimits,blkLimitsGC)
+  allocate(xcent(blkLimitsGC(HIGH, IAXIS)))
+  call Grid_getCellCoords(IAXIS, blockId, CENTER, .true., &
+       xcent, blkLimitsGC(HIGH, IAXIS))
+  allocate(ycent(blkLimitsGC(HIGH, JAXIS)))
+  call Grid_getCellCoords(JAXIS, blockId, CENTER, .true., &
+       ycent, blkLimitsGC(HIGH, JAXIS))
+  allocate(zcent(blkLimitsGC(HIGH, KAXIS)))
+  call Grid_getCellCoords(KAXIS, blockId, CENTER, .true., &
+       zcent, blkLimitsGC(HIGH, KAXIS))
+
+  !------------------------------------------------------------------------------
+
+  ! Loop over cells and set the initial state
+  do k = blkLimits(LOW,KAXIS),blkLimits(HIGH,KAXIS)
+     do j = blkLimits(LOW,JAXIS),blkLimits(HIGH,JAXIS)
+        do i = blkLimits(LOW,IAXIS),blkLimits(HIGH,IAXIS)
+
+           axis(IAXIS) = i
+           axis(JAXIS) = j
+           axis(KAXIS) = k
+
+           if(NDIM == 2) then
+              species = CHAM_SPEC
+              if ((xcent(i) >= sim_LL) .and. (xcent(i) <= sim_LR) .and. (ycent(j) <= sim_LT) .and. (ycent(j) >= sim_LB)) then
+                rho = sim_rhoChamIn
+                tele = sim_teleCham
+                tion = sim_tionCham
+                trad = sim_tradCham
+                vx = sim_VL
+              elseif ((xcent(i) >= sim_RL) .and. (xcent(i) <= sim_RR) .and. (ycent(j) <= sim_RT) .and. (ycent(j) >= sim_RB)) then
+                rho = sim_rhoChamIn
+                tele = sim_teleCham
+                tion = sim_tionCham
+                trad = sim_tradCham
+                vx = sim_VR
+              else
+                rho = sim_rhoChamOut
+                tele = sim_teleCham
+                tion = sim_tionCham
+                trad = sim_tradCham
+                vx = 0.0
+              end if
+           end if
+
+
+           call Grid_putPointData(blockId, CENTER, DENS_VAR, EXTERIOR, axis, rho)
+           call Grid_putPointData(blockId, CENTER, TEMP_VAR, EXTERIOR, axis, tele)
+           call Grid_putPointData(blockId, CENTER, VELX_VAR, EXTERIOR, axis, vx)
+#ifdef FLASH_3T
+           call Grid_putPointData(blockId, CENTER, TION_VAR, EXTERIOR, axis, tion)
+           call Grid_putPointData(blockId, CENTER, TELE_VAR, EXTERIOR, axis, tele)
+
+           ! Set up radiation energy density:
+           call RadTrans_mgdEFromT(blockId, axis, trad, tradActual)
+           call Grid_putPointData(blockId, CENTER, TRAD_VAR, EXTERIOR, axis, tradActual)
+#endif
+           if (NSPECIES > 0) then
+              ! Fill mass fractions in solution array if we have any SPECIES defined.
+              ! We put nearly all the mass into either the Xe material if XE_SPEC is defined,
+              ! or else into the first species.
+              do n = SPECIES_BEGIN,SPECIES_END
+                 if (n==species) then
+                    call Grid_putPointData(blockID, CENTER, n, EXTERIOR, axis, 1.0e0-(NSPECIES-1)*sim_smallX)
+                 else
+                    call Grid_putPointData(blockID, CENTER, n, EXTERIOR, axis, sim_smallX)
+                 end if
+              enddo
+           end if
+
+#ifdef BDRY_VAR
+           call Grid_putPointData(blockId, CENTER, BDRY_VAR, EXTERIOR, axis, -1.0)
+#endif
+        enddo
+     enddo
+  enddo
+  deallocate(xcent)
+  deallocate(ycent)
+  deallocate(zcent)
+
+  return
+
+end subroutine Simulation_initBlock
